4c4e4e1412ad667122caa2a83849fffc4741dfa4
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-06 13:56:54 -0500

    Paso2: terminado todo 2h

--- create.sql
+++ create.sql
@@ -4,14 +4,14 @@ id SERIAL PRIMARY KEY,
 name VARCHAR NOT NULL, 
 nationality VARCHAR, 
 birthdate DATE
-)
+);
 
 CREATE TABLE publishers(
 id SERIAL PRIMARY KEY,
 name VARCHAR NOT NULL, 
 annual_revenue INTEGER, 
 founded_year INTEGER
-)
+);
 
 CREATE TABLE books(
 id SERIAL PRIMARY KEY,
@@ -19,15 +19,15 @@ title VARCHAR NOT NULL,
 pages INTEGER NOT NULL CHECK(pages>0), 
 author_id INTEGER NOT NULL REFERENCES authors(id),
 publisher_id INTEGER NOT NULL REFERENCES publishers(id)
-)
+);
 
 CREATE TABLE genres(
 id SERIAL PRIMARY KEY,
-genre VARCHAR
-)
+genre VARCHAR NOT NULL
+);
 
 CREATE TABLE books_genres(
 id SERIAL PRIMARY KEY,
 genre_id INTEGER NOT NULL REFERENCES genres(id),
 book_id INTEGER NOT NULL REFERENCES books(id)
-)
+);
---
+++ insert_data.rb
@@ -0,0 +1,37 @@
+
+###### Paso 1: sin llenar datos, sale vacío los datos######
+#require "pg";
+#conn = PG.connect( dbname: 'booking' ) #Paso 1: Nos conectamos a la base de datos usando Ruby
+#result = conn.exec( "SELECT * FROM authors" ) #Paso 2: Podemos hacer llmadas a la base de datos
+# p conn
+# p result
+#p result.fields #["id", "name", "nationality", "birthdate"]
+#p result.values #[]
+
+###### formas de string para que conn.exec( %[INSERT INTO "authors_book"(name) VALUES ('AXEL')] )  acepten tables con nombre como por ejemplo authors_book ######
+
+#"Hola" = 'Hola' = %(Hola) = %("Hola") = %('Hola') = %["Hola"] = %['Hola']
+
+###### Paso2 :  Estamos llenadno datos con conn.exec( %[INSERT INTO authors(name) VALUES ('AXEL')] )###########
+
+#require "pg";
+# conn = PG.connect( dbname: 'booking' ) 
+# conn.exec( %[INSERT INTO authors(name) VALUES ('AXEL')] ) # el id no se necesita poner por ser una primar y se calcula automáticamnete, los demás datos dependiendo del caso se pueden poner 
+# # el del caso de arriba de authors solo se puso name ya que habíamos definido el valor como no nulo y los demás sí pueden ser nulos
+# result = conn.exec( "SELECT * FROM authors" ) 
+# p result.fields #["id", "name", "nationality", "birthdate"]
+# p result.values #[["1", "AXEL", nil, nil]]
+
+###### Paso3 :  usando el CSV de nuesto arhcivo books.csv######
+require "pg";
+require "csv";
+
+conn = PG.connect( dbname: 'booking' ) 
+CSV.foreach("books.csv", headers: true) do |row|
+    p row["author_name"]
+    # p row
+end
+# conn.exec( %[INSERT INTO authors(name) VALUES ('AXEL')] ) 
+# result = conn.exec( "SELECT * FROM authors" ) 
+# p result.fields #["id", "name", "nationality", "birthdate"]
+# p result.values #[["1", "AXEL", nil, nil]]
