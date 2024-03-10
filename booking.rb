c0694e7a93d8ea0bfc819e293ce212316a6db9b5
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-10 01:38:02 -0500

    terminado 6h  todo

--- booking.rb
+++ booking.rb
@@ -13,9 +13,9 @@ class Booking
             action, param = gets.chomp.split
             case action
             # when "1" then puts top_publishers_prueba
-            when "1" then puts top_publishers
-            when "2" then puts search_books(param)
-            when "3" then puts "Option 3"
+            when "1" then  top_publishers
+            when "2" then  search_books(param)
+            when "3" then  count_books(param)
             when "menu" then print_menu
             when "exit" then puts "Thank you for using booking"
             else puts "Invalid option"
@@ -56,10 +56,60 @@ def print_welcome
   end
   def search_books(param)
     # nos mandan "title=terrible" por liro
+    column_ref ={
+      "title" => "b.title",
+      "author" => "a.name",
+      "publisher" => "p.name"
+    }
     column, query = param.split('=')
-    result = @db.exec("SELECT * FROM books WHERE LOWER(#{column}) LIKE '%#{query.downcase}%'");
+    column = column_ref[column]
+    # result = @db.exec(%[SELECT * FROM books WHERE LOWER(#{column}) LIKE '%#{query.downcase}%']);
+    result = @db.exec(%[SELECT b.title, b.pages, a.name AS author, p.name AS publisher FROM books AS b JOIN authors AS a ON a.id = b.author_id JOIN publishers AS p ON p.id = b.publisher_id WHERE LOWER(#{column}) LIKE '%#{query.downcase}%']) # WHERE LOWER(#{column}) LIKE '%#{query.downcase}: LOWER convierte todo el título del libro en minúscula sin modificar lo que muestra la tabla solo en este caso y con esto puedes hacer esto  2 title=TeRRible sin modificar el title en minúscula sino solo el nombre del título  ;
+    #probar: SELECT LOWER(title) FROM books WHERE title LIKE '%Cover%';
+    #probar: SELECT * FROM books WHERE LOWER(title) LIKE '%cover%';
     table = Terminal::Table.new
     table.title ="Search Books"
+    table.headings = result.fields
+    table.rows = result.values
+    puts table
+  end
+  def count_books(param)
+   
+    case param
+    when "author"
+      result = @db.exec(%[
+        SELECT a.name AS author, COUNT(*) AS count_books
+        FROM books AS b
+        JOIN authors AS a ON a.id = b.author_id
+        GROUP BY author
+        ORDER BY count_books DESC;
+      ])
+    when "publisher"
+      result = @db.exec(%[
+        SELECT p.name AS publisher, COUNT(*) AS count_books
+        FROM books AS b
+        JOIN publishers AS p ON p.id = b.publisher_id
+        GROUP BY publisher
+        ORDER BY count_books DESC;
+      ])
+      # SELECT p.name AS publisher, COUNT(*) AS count_books
+      #   FROM publishers  AS p
+      #   JOIN books  AS b ON p.id = b.publisher_id
+      #   GROUP BY publisher
+      #   ORDER BY count_books DESC;
+    when "genre"
+      result = @db.exec(%[
+        SELECT g.genre, COUNT(*) AS count_books
+        FROM books AS b
+        JOIN books_genres AS bg ON b.id = bg.book_id
+        JOIN genres AS g ON g.id = bg.genre_id
+        GROUP BY genre
+        ORDER BY count_books DESC;
+      ])
+    end
+
+    table = Terminal::Table.new
+    table.title = "Count Books"
     table.headings = result.fields
     table.rows = result.values
     puts table
